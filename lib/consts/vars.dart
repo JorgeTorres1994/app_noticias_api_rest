f5996201600041260a3b3679447185544379fb89
Jorge Torres <torrespastor4@hotmail.com>
2025-08-07 03:41:49 -0500

    Implementacion de widgets, pagination, boton  dropdown, carga de articulos, efecto shimmer y  navegacion con transicion

--- lib/consts/vars.dart
+++ lib/consts/vars.dart
@@ -1,4 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 enum NewsType {
   topTrending,
   allNews,
 }
+
+enum SortByEnum {
+  relevancy, // articles more closely related to q come first.
+  popularity, // articles from popular sources and publishers come first.
+  publishedAt, // newest articles come first.
+}
+
+TextStyle smallTextStyle = GoogleFonts.montserrat(fontSize: 15);
--- lib/widgets/vertical_spacing.dart
+++ lib/widgets/vertical_spacing.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class VerticalSpacing extends StatelessWidget {
   const VerticalSpacing(this.height);
